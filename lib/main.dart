be4a9d62ae44f589054e032bc8ffcb5f2d3c6b82
Fred Jonason <fredjonason@gmail.com>
2019-08-28 16:54:17 +0800

    init commit, the DevFest app based on https://www.youtube.com/watch?v=gw272X66qvU&list=PLR2qQy0Zxs_Vju3tKnYgzbAhywHHPuDjQ

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'utils/devfest.dart';
+
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+  Devfest.prefs = await SharedPreferences.getInstance();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevFest",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

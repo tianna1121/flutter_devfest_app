c5751b50a9ddc285173ba8999b7c103665f80cbb
Fred Jonason <fredjonason@gmail.com>
2019-08-29 15:41:22 +0800

    add devFestTexts and socalActions

--- lib/home/home_widgets/home_front.dart
+++ lib/home/home_widgets/home_front.dart
@@ -2,8 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:flutter_devfest/config/index.dart';
 import 'package:flutter_devfest/universal/image_card.dart';
 import 'package:flutter_devfest/utils/devfest.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeFront extends StatelessWidget {
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+
+  List<Widget> devFestTexts(context) => [
+        Text(
+          Devfest.welcomeText,
+          style: Theme.of(context).textTheme.headline,
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          Devfest.descText,
+          style: Theme.of(context).textTheme.caption,
+          textAlign: TextAlign.center,
+        )
+      ];
+
+  Widget socialActions(context) => FittedBox(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(FontAwesomeIcons.facebookF),
+              onPressed: () async {
+                await _launchURL("https://facebook.com/imthepk");
+              },
+            ),
+            IconButton(
+              icon: Icon(FontAwesomeIcons.twitter),
+              onPressed: () async {
+                await _launchURL("https://twitter.com/imthepk");
+              },
+            ),
+            IconButton(
+              icon: Icon(FontAwesomeIcons.linkedin),
+              onPressed: () async {
+                await _launchURL("https://linkedin.com/imthepk");
+              },
+            ),
+            IconButton(
+              icon: Icon(FontAwesomeIcons.youtube),
+              onPressed: () async {
+                await _launchURL("https://youtube.com/mtechviral");
+              },
+            ),
+            IconButton(
+              icon: Icon(FontAwesomeIcons.meetup),
+              onPressed: () async {
+                await _launchURL("https://meetup.com");
+              },
+            ),
+            IconButton(
+              icon: Icon(FontAwesomeIcons.envelope),
+              onPressed: () async {
+                var emailUrl =
+                    '''mailto:mtechviral@gmail.com?subject=Support Needed For DevFest''';
+                var out = Uri.encodeFull(emailUrl);
+                await _launchURL(out);
+              },
+            ),
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -16,6 +89,21 @@ class HomeFront extends StatelessWidget {
                 img: ConfigBloc().darkModeOn
                     ? Devfest.banner_dark
                     : Devfest.banner_light),
+            SizedBox(
+              height: 20,
+            ),
+            ...devFestTexts(context),
+            SizedBox(
+              height: 20,
+            ),
+            socialActions(context),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              Devfest.app_version,
+              style: Theme.of(context).textTheme.caption.copyWith(fontSize: 10),
+            )
           ],
         ),
       ),
